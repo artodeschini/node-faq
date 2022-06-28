880fe217d291f00a38bea4b32f04b2a02c0afb9b
Artur Todeschini <artodeschini@gmail.com>
2022-06-28 10:55:01 -0300

    alterado para utilizar posgres para dar continuidade

--- database/database.js
+++ database/database.js
@@ -1,8 +1,38 @@
-const sequelize = require("sequelize");
+const { Sequelize } = require('sequelize');
 
-const connection = new sequelize("guiaperguntas", "root", "root", {
-    host: 'localhost',
-    dialect: 'mysql'
+//npm install --save sequelize
+
+// # One of the following:
+// $ npm install --save pg pg-hstore # Postgres
+// $ npm install --save mysql2
+// $ npm install --save mariadb
+// $ npm install --save sqlite3
+// $ npm install --save tedious # Microsoft SQL Server
+
+// Option 1: Passing a connection URI
+//const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+//const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/guiaperguntas'); // Example for postgres
+
+// Option 2: Passing parameters separately (sqlite)
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// });
+
+// Option 3: Passing parameters separately (other dialects)
+const connection = new Sequelize('guiaperguntas', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 });
+
+
+
+
+// const sequelize = require("sequelize");
+
+// const connection = new sequelize("guiaperguntas", "root", "root", {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
 
 module.exports = connection; 
